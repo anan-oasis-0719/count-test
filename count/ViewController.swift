0425a56d16f0e3714ae228d799f7248ca9816157
anan onda <anan.oasis.0719@gmail.com>
2016-01-15 19:17:59 +0900

    Initial Commit

---
+++ count/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  count
+//
+//  Created by onda anan on 2016/01/15.
+//  Copyright © 2016年 onda anan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
